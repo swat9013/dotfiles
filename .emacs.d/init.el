02778c0c36443ea17682bb188dd00e3b5b6248ba
swat9013 <12694496+swat9013@users.noreply.github.com>
2015-08-20 11:38:34 +0900

    emacs update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -59,18 +59,18 @@
 (sml/setup)
 
 (require 'ace-jump-mode)
-;; (define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
-(defun add-keys-to-ace-jump-mode (prefix c &optional mode)
-  (define-key global-map
-    (read-kbd-macro (concat prefix (string c)))
-    `(lambda ()
-       (interactive)
-       (funcall (if (eq ',mode 'word)
-                    #'ace-jump-word-mode
-                  #'ace-jump-char-mode) ,c))))
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+;; (defun add-keys-to-ace-jump-mode (prefix c &optional mode)
+;;   (define-key global-map
+;;     (read-kbd-macro (concat prefix (string c)))
+;;     `(lambda ()
+;;        (interactive)
+;;        (funcall (if (eq ',mode 'word)
+;;                     #'ace-jump-word-mode
+;;                   #'ace-jump-char-mode) ,c))))
 
-(loop for c from ?0 to ?9 do (add-keys-to-ace-jump-mode "H-" c 'word))
-(loop for c from ?a to ?z do (add-keys-to-ace-jump-mode "H-" c 'word))
+;; (loop for c from ?0 to ?9 do (add-keys-to-ace-jump-mode "H-" c 'word))
+;; (loop for c from ?a to ?z do (add-keys-to-ace-jump-mode "H-" c 'word))
 
 (require 'helm)
 (helm-mode 1)
