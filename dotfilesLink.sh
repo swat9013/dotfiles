d547075117cbd2af51df76e9b7299cca95ad8281
swat9013 <12694496+swat9013@users.noreply.github.com>
2015-10-19 10:28:39 +0900

    [modify] ssh-agent config

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -5,6 +5,7 @@ ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.rspec ~/.rspec
 ln -sf ~/dotfiles/.screenrc ~/.screenrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.zlogout ~/.zlogout
 ln -f ~/dotfiles/my-agnoster.zsh-theme ~/dotfiles/oh-my-zsh/themes/my-agnoster.zsh-theme
 ln -f ~/dotfiles/powerline.zsh-theme ~/dotfiles/oh-my-zsh/themes/powerline.zsh-theme
 ln -snf ~/dotfiles/zsh-syntax-highlighting ~/dotfiles/oh-my-zsh/custom/plugins/zsh-syntax-highlighting
