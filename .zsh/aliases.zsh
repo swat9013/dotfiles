2343ef405676598a5823f195e26791974d04e6d5
swat9013 <12694496+swat9013@users.noreply.github.com>
2015-11-20 11:44:55 +0900

    [add] alias

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,7 +1,18 @@
 ### Aliases ###
+alias ..='cd ..'
+alias ld='ls -ld'          # Show info about the directory
+alias lla='ls -lAF'        # Show hidden all files
+alias ll='ls -lF'          # Show long file information
+alias l='ls -1F'           # Show long file information
+alias la='ls -AF'          # Show hidden files
+alias lx='ls -lXB'         # Sort by extension
+alias lk='ls -lShr'         # Sort by size, biggest last
+alias lc='ls -ltcr'        # Sort by and show change time, most recent last
+alias lu='ls -ltur'        # Sort by and show access time, most recent last
+alias lt='ls -ltr'         # Sort by date, most recent last
+alias lr='ls -lR'          # Recursive ls
+alias grep='grep --color=auto'
 alias history='history -E'
-alias ll='ls -l'
-alias la='ls -a'
 alias s='screen'
 alias emacs-kill='emacsclient -e "(kill-emacs)"'
 alias zshrc='$EDITOR_CMD ~/.zshrc'
@@ -14,6 +25,7 @@ alias ssh-aa='eval `ssh-agent -s` ; ssh-add'
 alias less="less -qnR"
 alias tm="tmux"
 alias m="mux"
+alias relogin='exec $SHELL -l'
 
 #colordiff設定
 if [[ -x `which colordiff` ]]; then
