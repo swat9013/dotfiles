b1e11295fd145cab65415819eda04159223fe972
swat9013 <wattan1030@gmail.com>
2015-11-20 14:22:48 +0900

    [modify] mac support

--- .zsh/keybinds.zsh
+++ .zsh/keybinds.zsh
@@ -4,38 +4,6 @@
 ## zsh のキーバインドを環境変数 EDITOR に関わらず emacs 風にする
 bindkey -e
 
-## C-^ で一つ上のディレクトリへ
-function cdup() {
-    echo
-    cd ..
-    echo
-    zle reset-prompt
-}
-zle -N cdup
-bindkey '^^' cdup
-
-
-function do_enter() {
-    if [ -n "$BUFFER" ]; then
-        zle accept-line
-        return 0
-    fi
-    echo
-    ls
-    # ↓おすすめ
-    # ls_abbrev
-    if [ "$(git rev-parse --is-inside-work-tree 2> /dev/null)" = 'true' ]; then
-        # echo -e "\e[0;33m--- git status ---\e[0m"
-        echo
-        git status --short --branch
-    fi
-    echo
-    zle reset-prompt
-    return 0
-}
-zle -N do_enter
-bindkey '^m' do_enter
-
 function peco-select-history() {
     local tac
     if which tac > /dev/null; then
@@ -51,3 +19,38 @@ function peco-select-history() {
 }
 zle -N peco-select-history
 bindkey '^r' peco-select-history
+
+
+if [ "${OS}" != "darwin" ]; then
+    ## C-^ で一つ上のディレクトリへ
+    function cdup() {
+        echo
+        cd ..
+        echo
+        zle reset-prompt
+    }
+    zle -N cdup
+    bindkey '^^' cdup
+
+
+    function do_enter() {
+        if [ -n "$BUFFER" ]; then
+            zle accept-line
+            return 0
+        fi
+        echo
+        ls
+        # ↓おすすめ
+        # ls_abbrev
+        if [ "$(git rev-parse --is-inside-work-tree 2> /dev/null)" = 'true' ]; then
+            # echo -e "\e[0;33m--- git status ---\e[0m"
+            echo
+            git status --short --branch
+        fi
+        echo
+        zle reset-prompt
+        return 0
+    }
+    zle -N do_enter
+    bindkey '^m' do_enter
+fi
