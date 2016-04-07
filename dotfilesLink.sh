efb484551fc7f2a5a95ce7002cbb74c829c4c905
swat9013 <12694496+swat9013@users.noreply.github.com>
2016-04-07 20:14:55 +0900

    [add] install.sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
-ln -snf ~/dotfiles/.emacs.d ~/.emacs.d
-ln -sf ~/dotfiles/.colordiffrc ~/.colordiffrc
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/.rspec ~/.rspec
-ln -sf ~/dotfiles/.screenrc ~/.screenrc
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.zshenv ~/.zshenv
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/.zlogout ~/.zlogout
+cd ~/.dotfiles
+
+for f in .??*
+do
+    [ "$f" = ".git" ] && continue
+    [ "$f" = ".gitignore" ] && continue
+    [ "$f" = ".gitmodule" ] && continue
+    [ "$f" = ".gitconfig.local.sample" ] && continue
+
+    ln -snfv "$HOME"/.dotfiles/"$f" "$HOME"/"$f"
+done
+
 ln -f ~/dotfiles/my-agnoster.zsh-theme ~/dotfiles/oh-my-zsh/themes/my-agnoster.zsh-theme
 ln -f ~/dotfiles/powerline.zsh-theme ~/dotfiles/oh-my-zsh/themes/powerline.zsh-theme
 ln -snf ~/dotfiles/zsh-syntax-highlighting ~/dotfiles/oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+if [ ! -e "$HOME"/.gitconfig.local ]; then
+    cp .gitconfig.local.sample "$HOME"/.gitconfig.local
+fi
