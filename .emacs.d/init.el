056e5bb4738f7ee26914379d81fc44e56cf03fb4
swat9013 <wattan1030@gmail.com>
2016-12-06 03:16:03 +0900

    [fix] emacs bug

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -305,5 +305,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-comment-tag ((t (:foreground "brightblue"))))
- '(font-lock-function-name-face ((t (:foreground "color-33")))))
+ '(font-lock-function-name-face ((t (:foreground "color-33"))))
  '(web-mode-html-tag-bracket-face ((t (:foreground "cyan")))))
