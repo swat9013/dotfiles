1925280ef6e54393d8857d9ae5f9daf00ef8eacc
swat9013 <wattan1030@gmail.com>
2016-12-05 16:01:22 +0900

    [fix] support emacs 25

--- .emacs.d/init.d/init-config.el
+++ .emacs.d/init.d/init-config.el
@@ -9,7 +9,7 @@
 
 ;;スクロールの設定
 (xterm-mouse-mode t)
-(mouse-wheel-mode t)
+;; (mouse-wheel-mode t)
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
 (setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
 (setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
