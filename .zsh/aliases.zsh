dcf9cf12b9ceb591a898158d26d788932f26c7a2
swat9013 <wattan1030@gmail.com>
2017-08-25 15:47:01 +0900

    sort alias

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,44 +1,44 @@
 ### Aliases ###
-alias ..='cd ..'
-alias ld='ls -ld'          # Show info about the directory
-alias lla='ls -lAF'        # Show hidden all files
-alias ll='ls -lF'          # Show long file information
-alias l='ls -1F'           # Show long file information
-alias la='ls -AF'          # Show hidden files
-alias lx='ls -lXB'         # Sort by extension
-alias lk='ls -lShr'         # Sort by size, biggest last
-alias lc='ls -ltcr'        # Sort by and show change time, most recent last
-alias lu='ls -ltur'        # Sort by and show access time, most recent last
-alias lt='ls -ltr'         # Sort by date, most recent last
-alias lr='ls -lR'          # Recursive ls
-alias grep='grep --color=auto'
-alias history='history -E'
-alias s='screen'
-alias emacs-kill='emacsclient -e "(kill-emacs)"'
-alias zshrc='$EDITOR_CMD ~/.zshrc'
-alias tf='tail -f'
-alias g='git'
-alias db_rollback='rake db:rollback'
-alias db_migrate='rake db:migrate'
-alias tail_log='tail -f *.log'
-alias ssh-aa='eval `ssh-agent -s` ; ssh-add'
-alias less="less -qnR"
-alias tm="tmux"
-alias tmkss="tmux kill-session"
-alias tmksr="tmux kill-server"
-alias m="tmuxinator"
-alias relogin='exec $SHELL -l'
 alias -g L='`git log --decorate --oneline | peco | cut -d" " -f1`'
 alias -g LA='`git log --decorate --oneline --all | peco | cut -d" " -f1`'
 alias -g R='`git reflog | peco | cut -d" " -f1`'
-alias dotfiles='cd ~/.dotfiles'
-alias sshdir='cd ~/.ssh'
-alias p='python'
-alias ps-grep="ps aux | grep"
+alias ..='cd ..'
+alias db_migrate='rake db:migrate'
+alias db_rollback='rake db:rollback'
+alias dclog='COMPOSE_HTTP_TIMEOUT=30000 docker-compose logs -f'
 alias dcr='docker-compose run'
 alias dcrw='docker-compose run web'
 alias dcud='docker-compose up -d'
-alias dclog='COMPOSE_HTTP_TIMEOUT=30000 docker-compose logs -f'
+alias dotfiles='cd ~/.dotfiles'
+alias emacs-kill='emacsclient -e "(kill-emacs)"'
+alias g='git'
+alias grep='grep --color=auto'
+alias history='history -E'
+alias l='ls -1F'           # Show long file information
+alias la='ls -AF'          # Show hidden files
+alias lc='ls -ltcr'        # Sort by and show change time, most recent last
+alias ld='ls -ld'          # Show info about the directory
+alias less="less -qnR"
+alias lk='ls -lShr'         # Sort by size, biggest last
+alias ll='ls -lF'          # Show long file information
+alias lla='ls -lAF'        # Show hidden all files
+alias lr='ls -lR'          # Recursive ls
+alias lt='ls -ltr'         # Sort by date, most recent last
+alias lu='ls -ltur'        # Sort by and show access time, most recent last
+alias lx='ls -lXB'         # Sort by extension
+alias m="tmuxinator"
+alias p='python'
+alias ps-grep="ps aux | grep"
+alias relogin='exec $SHELL -l'
+alias s='screen'
+alias ssh-aa='eval `ssh-agent -s` ; ssh-add'
+alias sshdir='cd ~/.ssh'
+alias sshdir='cd ~/.ssh'
+alias tf='tail -f'
+alias tm="tmux"
+alias tmksr="tmux kill-server"
+alias tmkss="tmux kill-session"
+alias zshrc='$EDITOR_CMD ~/.zshrc'
 
 #colordiff設定
 if [[ -x `which colordiff` ]]; then
