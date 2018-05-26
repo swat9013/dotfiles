fc4627f9c1e519c88321fe886557dcae3fea4326
swat9013 <wattan1030@gmail.com>
2018-05-26 22:54:25 +0900

    add migemo for emacs

---
+++ .emacs.d/init.d/mac-config.el
@@ -0,0 +1,20 @@
+(use-package helm-gtags
+  :init
+  (add-hook 'ruby-mode-hook (lambda () (helm-gtags-mode)))
+  :config
+  (setq helm-gtags-path-style 'root)
+  (setq helm-gtags-auto-update t))
+(bind-key* "M-." 'helm-gtags-find-tag)
+(bind-key* "M-," 'helm-gtags-pop-stack)
+(bind-key* "M-r" 'helm-gtags-find-rtag)
+
+(use-package migemo
+  :config
+  (setq migemo-options '("-q" "--emacs"))
+  (setq migemo-user-dictionary nil)
+  (setq migemo-regex-dictionary nil)
+  (setq migemo-coding-system 'utf-8-unix)
+  (setq migemo-command "cmigemo")
+  (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+  (migemo-init)
+)
