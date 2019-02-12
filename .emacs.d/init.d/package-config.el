fde64ffa48984db2723a4d05eb36b2b4b2506e01
swat9013 <12694496+swat9013@users.noreply.github.com>
2019-02-12 18:13:24 +0900

    add counsel+ivy

--- .emacs.d/init.d/package-config.el
+++ .emacs.d/init.d/package-config.el
@@ -2,20 +2,20 @@
   :config
   (editorconfig-mode 1))
 
-(use-package wgrep
-  :ensure t)
-(use-package wgrep-ag
-  :ensure t)
-(use-package wgrep-helm
-  :ensure t)
-(setq wgrep-enable-key "e")
-(setq wgrep-auto-save-buffer t)
-(setq wgrep-change-readonly-file t)
+;; (use-package wgrep
+;;   :ensure t)
+;; (use-package wgrep-ag
+;;   :ensure t)
+;; (use-package wgrep-helm
+;;   :ensure t)
+;; (setq wgrep-enable-key "e")
+;; (setq wgrep-auto-save-buffer t)
+;; (setq wgrep-change-readonly-file t)
 
 (use-package smartrep)
 (smartrep-define-key
-    global-map "C-c" '(("-" . 'goto-last-change)
-                       ("+" . 'goto-last-change-reverse)))
+  global-map "C-c" '(("-" . 'goto-last-change)
+                      ("+" . 'goto-last-change-reverse)))
 
 (use-package auto-package-update)
 (auto-package-update-maybe)
@@ -48,7 +48,7 @@
     (interactive)
     ;; Don't call desktop-save-in-desktop-dir, as it prints a message.
     (if (eq (desktop-owner) (emacs-pid))
-        (desktop-save desktop-dirname)))
+      (desktop-save desktop-dirname)))
   (add-hook 'auto-save-hook 'my-desktop-save))
 
 (use-package git-gutter+
@@ -61,9 +61,9 @@
   :config
   (global-anzu-mode 1)
   (custom-set-variables
-   '(anzu-deactivate-region t)
-   '(anzu-mode-lighter "")
-   '(anzu-search-threshold 100)))
+    '(anzu-deactivate-region t)
+    '(anzu-mode-lighter "")
+    '(anzu-search-threshold 100)))
 (bind-key* "M-%" 'anzu-query-replace)
 (bind-key* "M-$" 'anzu-query-replace-regexp)
 
@@ -84,16 +84,16 @@
   (setq sml/theme 'light)
   (sml/setup))
 
-(use-package ace-jump-mode
-  :bind(("C-c SPC" . ace-jump-mode)))
+;; (use-package ace-jump-mode
+;;   :bind(("C-c SPC" . ace-jump-mode)))
 
 (use-package rainbow-delimiters)
 (use-package magit)
 (bind-key* "C-x m" 'magit-status)
 
-(use-package ido
-  :config
-  (ido-mode t))
+;; (use-package ido
+;;   :config
+;;   (ido-mode t))
 
 (use-package undo-tree
   :diminish undo-tree-mode
@@ -111,47 +111,49 @@
 ;;
 ;; helm projectile
 ;;
-(use-package helm
-  ;; :diminish helm-mode
-  :config
-  (ido-mode -1)
-  (helm-mode 1)
-  (setq helm-buffers-fuzzy-matching t
-        helm-recentf-fuzzy-match    t)
-  (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to do persistent action
-  (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
-  (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
-  :bind(("C-x C-o" . helm-mini)
-        ("M-y" . helm-show-kill-ring)
-        ("M-x" . helm-M-x)
-        ("C-x C-f" . helm-find-files)
-        ("C-x C-t" . helm-recentf)))
 
-
-(use-package helm-config)
-(use-package helm-files)
-;; (use-package helm-descbinds
+;; (use-package helm
+;;   ;; :diminish helm-mode
 ;;   :config
-;;   (helm-descbinds-mode))
-(use-package helm-ag
-  :config
-  (setq helm-ag-base-command "rg --vimgrep --no-heading")
-  (setq helm-ag-insert-at-point 'symbol)
-  (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to do persistent action
-  (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
-  :bind(("C-M-s" . helm-ag-this-file)))
+;;   (ido-mode -1)
+;;   (helm-mode 1)
+;;   (setq helm-buffers-fuzzy-matching t
+;;         helm-recentf-fuzzy-match    t)
+;;   (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to do persistent action
+;;   (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+;;   (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+;;   :bind(("C-x C-o" . helm-mini)
+;;         ("M-y" . helm-show-kill-ring)
+;;         ("M-x" . helm-M-x)
+;;         ("C-x C-f" . helm-find-files)
+;;         ("C-x C-t" . helm-recentf)))
+
+
+;; (use-package helm-config)
+;; (use-package helm-files)
+;; ;; (use-package helm-descbinds
+;; ;;   :config
+;; ;;   (helm-descbinds-mode))
+;; (use-package helm-ag
+;;   :config
+;;   (setq helm-ag-base-command "rg --vimgrep --no-heading")
+;;   (setq helm-ag-insert-at-point 'symbol)
+;;   (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to do persistent action
+;;   (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+;;   :bind(("C-M-s" . helm-ag-this-file)))
 
 (use-package projectile
   :diminish projectile-mode
   :config
   (projectile-global-mode t)
-  (setq projectile-completion-system 'helm)
-  (helm-projectile-on)
-  (defun helm-projectile-rg ()
-    (interactive)
-    (helm-ag (projectile-project-root))))
-(bind-key* "C-c C-f" 'helm-projectile-find-file)
-(bind-key* "C-c C-g" 'helm-projectile-rg)
+  (setq projectile-completion-system 'ivy))
+;; (setq projectile-completion-system 'helm)
+;; (helm-projectile-on)
+;; (defun helm-projectile-rg ()
+;;   (interactive)
+;;   (helm-ag (projectile-project-root))))
+;; (bind-key* "C-c C-f" 'helm-projectile-find-file)
+;; (bind-key* "C-c C-g" 'helm-projectile-rg)
 (bind-key* "C-c C-t" 'projectile-recentf)
 (bind-key* "C-c C-r" 'projectile-replace)
 
@@ -164,13 +166,61 @@
   (setq recentf-auto-save-timer (run-with-idle-timer 30 t 'recentf-save-list)))
 
 ;;
+;; counsel
+;;
+
+(use-package swiper
+  :ensure t
+  :config
+  (defvar swiper-include-line-number-in-search t)
+  )
+(bind-key* "C-s" 'swiper)
+
+(use-package avy-migemo
+  :config
+  (avy-migemo-mode 1)
+  (require 'avy-migemo-e.g.swiper))
+
+(use-package counsel
+  :ensure t
+  :config
+  (defvar counsel-find-file-ignore-regexp (regexp-opt '("./" "../")))
+  (ivy-mode 1)
+  (setq qivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t)
+  (setq ivy-height 20) ;; minibufferのサイズを拡大
+  (setq ivy-extra-directories nil)
+  (setq ivy-re-builders-alist
+    '((t . ivy--regex-plus))))
+(bind-key* "M-x" 'counsel-M-x)
+(bind-key* "C-x C-f" 'counsel-find-file)
+(bind-key* "C-x C-t" 'counsel-recentf)
+(bind-key* "M-y" 'counsel-yank-pop)
+
+(use-package counsel-projectile)
+(bind-key* "C-c C-f" 'counsel-projectile-find-file)
+(bind-key* "C-c C-g" 'counsel-projectile-rg)
+(bind-key* "C-c g" 'counsel-projectile-git-grep)
+(bind-key* "C-x C-o" 'counsel-projectile-switch-to-buffer)
+
+(use-package smart-jump :ensure t)
+(bind-key* "M-d" 'smart-jump-go)
+
+;;
 ;; Ruby on Rails
 ;;
 (use-package robe
   :init
   (add-hook 'ruby-mode-hook 'robe-mode)
-  (custom-set-variables '(robe-completing-read-func 'helm-robe-completing-read))
+  ;; (custom-set-variables '(robe-completing-read-func 'helm-robe-completing-read))
   ;; (add-hook 'robe-mode-hook 'ac-robe-setup)
+  )
+
+(use-package symbol-overlay
+  :ensure t
+  :init
+  (add-hook 'prog-mode-hook #'symbol-overlay-mode)
+  (add-hook 'markdown-mode-hook #'symbol-overlay-mode)
   )
 
 ;;(use-package ruby-block
@@ -182,24 +232,24 @@
 
 (use-package ruby-mode
   :mode (("\\.rb$" . ruby-mode)
-         ("Capfile$" . ruby-mode)
-         ("Gemfile$" . ruby-mode)
-         ("[Ra]kefile$" . ruby-mode)
-         ("\\.rake$" . ruby-mode)
-         ("Berksfile$" . ruby-mode)
-         ("\\.rabl$" . ruby-mode))
+          ("Capfile$" . ruby-mode)
+          ("Gemfile$" . ruby-mode)
+          ("[Ra]kefile$" . ruby-mode)
+          ("\\.rake$" . ruby-mode)
+          ("Berksfile$" . ruby-mode)
+          ("\\.rabl$" . ruby-mode))
   :config
   (defun ruby-mode-set-encoding () nil)
   (setq ruby-deep-indent-paren-style nil)
   (defadvice ruby-indent-line (after unindent-closing-paren activate)
     (let ((column (current-column))
-          indent offset)
+           indent offset)
       (save-excursion
         (back-to-indentation)
         (let ((state (syntax-ppss)))
           (setq offset (- column (current-column)))
           (when (and (eq (char-after) ?\))
-                     (not (zerop (car state))))
+                  (not (zerop (car state))))
             (goto-char (cadr state))
             (setq indent (current-indentation)))))
       (when indent
@@ -217,11 +267,11 @@
 ;;
 (use-package web-mode
   :mode (("\\.phtml$"     . web-mode)
-         ("\\.tpl\\.php$" . web-mode)
-         ("\\.jsp$"       . web-mode)
-         ("\\.as[cp]x$"   . web-mode)
-         ("\\.erb$"       . web-mode)
-         ("\\.html?$"     . web-mode))
+          ("\\.tpl\\.php$" . web-mode)
+          ("\\.jsp$"       . web-mode)
+          ("\\.as[cp]x$"   . web-mode)
+          ("\\.erb$"       . web-mode)
+          ("\\.html?$"     . web-mode))
   :config
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
@@ -229,7 +279,7 @@
   (setq web-mode-style-padding 0)
   (setq web-mode-script-padding 0)
   (add-hook 'web-mode-hook
-            (lambda () (set-variable 'tab-width 2))))
+    (lambda () (set-variable 'tab-width 2))))
 
 (use-package shell-script-mode
   :mode (("\\.zsh" . shell-script-mode)))
@@ -250,8 +300,8 @@
 
 (use-package yaml-mode
   :mode (("¥¥.yml$" . yaml-mode)
-         ("\\.dig" . yaml-mode)
-         ("\\.yml.liquid" . yaml-mode)))
+          ("\\.dig" . yaml-mode)
+          ("\\.yml.liquid" . yaml-mode)))
 
 (use-package markdown-mode
   :config
@@ -264,9 +314,9 @@
 
 (use-package ssh-config-mode
   :mode ((".ssh/config\\'"       . ssh-config-mode)
-         ("sshd?_config\\'"      . ssh-config-mode)
-         ("known_hosts\\'"       . ssh-known-hosts-mode)
-         ("authorized_keys2?\\'" . ssh-authorized-keys-mode))
+          ("sshd?_config\\'"      . ssh-config-mode)
+          ("known_hosts\\'"       . ssh-known-hosts-mode)
+          ("authorized_keys2?\\'" . ssh-authorized-keys-mode))
   :init
   (add-hook 'ssh-config-mode-hook 'turn-on-font-lock))
 
