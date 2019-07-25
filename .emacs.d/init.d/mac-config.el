97b4bfaf90d57d96774b61613040ca6bdab417b9
swat9013 <12694496+swat9013@users.noreply.github.com>
2019-07-25 15:55:48 +0900

    fix emacs package

--- .emacs.d/init.d/mac-config.el
+++ .emacs.d/init.d/mac-config.el
@@ -8,18 +8,18 @@
 ;; (bind-key* "M-," 'helm-gtags-pop-stack)
 ;; (bind-key* "M-r" 'helm-gtags-find-rtag)
 
-(use-package migemo
-  :config
-  (setq migemo-options '("-q" "--emacs"))
-  (setq migemo-user-dictionary nil)
-  (setq migemo-regex-dictionary nil)
-  (setq migemo-coding-system 'utf-8-unix)
-  (setq migemo-command "cmigemo")
-  (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
-  (migemo-init)
-)
+;; (use-package migemo
+;;   :config
+;;   (setq migemo-options '("-q" "--emacs"))
+;;   (setq migemo-user-dictionary nil)
+;;   (setq migemo-regex-dictionary nil)
+;;   (setq migemo-coding-system 'utf-8-unix)
+;;   (setq migemo-command "cmigemo")
+;;   (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+;;   (migemo-init)
+;; )
 
-(use-package avy-migemo
-  :config
-  (avy-migemo-mode 1)
-  (require 'avy-migemo-e.g.swiper))
+;; (use-package avy-migemo
+;;   :config
+;;   (avy-migemo-mode 1)
+;;   (require 'avy-migemo-e.g.swiper))
