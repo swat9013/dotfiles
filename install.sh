eb77330f04bf32d80f36aaaa6f9f688448ff154b
swat9013 <12694496+swat9013@users.noreply.github.com>
2022-01-28 23:18:33 +0900

    brew bundle

--- install.sh
+++ install.sh
@@ -16,10 +16,12 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+sh dotfilesLink.sh
+
 if [ "$(uname)" == 'Darwin' ]; then
     echo "=== Mac OS==="
     if  [ `which brew` ]; then
-        brew bundle
+        brew bundle --global --no-lock --cleanup
     else
         echo "home brew requrired"
         echo "https://brew.sh/index_ja.html"
@@ -36,5 +38,3 @@ elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then
 else
     echo "Your platform ($(uname -a))"
 fi
-
-sh dotfilesLink.sh
