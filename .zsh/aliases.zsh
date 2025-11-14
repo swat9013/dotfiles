c02675b76468efa221219450769f4c379b343c85
swat9013 <12694496+swat9013@users.noreply.github.com>
2025-11-14 10:51:27 +0900

    AIが混乱するので削除

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -138,11 +138,3 @@ alias setup="./script/setup.sh"
 # python
 #
 alias pip-upgrade-all="pip list -o | tail -n +3 | awk '{ print \$1 }' | xargs pip install -U"
-
-
-#
-# mac
-#
-if [ "$(uname)" = 'Darwin' ]; then
-alias sed="gsed"
-fi
